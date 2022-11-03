df2b890df08253252a38e58e8fbadd6a8128b20c
Thomas Willson <thomas.willson@epitech.eu>
2022-11-03 11:25:07 +0100

    add: schema with fields that we require to add data...

---
+++ models/model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    type: {
+        required: true,
+        type: String
+    },
+    price: {
+        required: true,
+        type: Number
+    },
+    rating: {
+        required: true,
+        type: Number
+    },
+    warranty_years: {
+        required: true,
+        type: Number
+    },
+    available: {
+        required: true,
+        type: Boolean
+    },
+})
+
+module.exports = mongoose.model('Data', dataSchema)
